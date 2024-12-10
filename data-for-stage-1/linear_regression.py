578da539d81cedfc4dceb28c7377b1afa16bd6cc
Jason <Jason1@jelincic.com>
2024-12-10 14:42:10 +0800

    feat: new functions and files for running mean testing

--- data-for-stage-1/linear_regression.py
+++ data-for-stage-1/linear_regression.py
@@ -5,10 +5,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import csv
 
-data = pd.read_csv('./train_data.csv')
+data = pd.read_csv('../data-exploration/train_test_data.csv')
 test_data = pd.read_csv('./same_season_test_data.csv')
 
-def output_results(wlin, filename='results.csv'):
+def output_results(test_x, wlin, filename='results.csv'):
 
   results = np.dot(test_x, wlin)
 
@@ -30,7 +30,7 @@ def output_results(wlin, filename='results.csv'):
         writer.writerow([i, False])
 
 # Preprocessing function
-def preprocess_data(data):
+def preprocess_train_data(data):
   # Drop non-predictive columns
   drop_cols = ['id', 'date']
   data = data.drop(columns=drop_cols, errors='ignore')
@@ -47,7 +47,7 @@ def preprocess_data(data):
   data = data.fillna(data.median(numeric_only=True))
 
   # Split features and target
-  x = data.drop(columns=['home_team_win']).filter(items = [
+  x = data.drop(columns=['home_team_win'], errors='ignore').filter(items = [
     "away_pitching_SO_batters_faced_10RA",
     "home_pitching_earned_run_avg_mean",
     "away_pitcher_SO_batters_faced_mean",
@@ -151,10 +151,7 @@ def preprocess_test_data(data, features):
     
   return X_train_with_bias, label_encoders
 
-x, y, label_encoders = preprocess_data(data)
-test_x, label_encoders_2 = preprocess_test_data(test_data, 16)
-
-def selectData(features, sampleSize):
+def selectData(x, y, features, sampleSize):
 
   X_train = np.array([[example[i] for i in range(1, features+1)] for example in x])
 
@@ -178,33 +175,64 @@ def train(x, y):
 
   return wlin
 
-def linearRegression(features, N):
+def linearRegression(x, y, test_x, features, N):
 
-  x_train, x_test, y_train, y_test = selectData(features, N)
+  x_train, x_test, y_train, y_test = selectData(x, y, features, N)
 
   wlin = train(x_train, y_train)
 
   y_train_pred = np.dot(x_train, wlin) 
 
-  Ein = np.mean((y_train_pred - y_train) ** 2)
+  Ein = np.sum(y_train_pred != y_train)
 
   y_test_pred = np.dot(x_test, wlin)
   
   accuracy = np.mean(np.sign(y_test_pred) == y_test)
-  Eout = np.mean((y_test_pred - y_test) ** 2)
+  Eout = np.sum(y_test_pred != y_test)
 
-  output_results(wlin)
+  output_results(test_x, wlin)
 
   return Ein, Eout, accuracy
 
+def preprocess_data(data, features):
+  x, y, label_encoders = preprocess_train_data(data)
+  test_x, label_encoders_2 = preprocess_test_data(test_data, features)
+
+  return x, y, test_x
+
+def plot_output(Ns, Eins, Eouts, accs):
+  print('average accuracy:', np.mean(accs))
+  print('max accuracy:', np.max(accs))
+  plt.title('N VS Ein and Eout')
+  plt.xlabel('N')
+  plt.ylabel('Ein and Eout')
+
+  # Show the plot
+  plt.plot(Ns, Eins, color='red', alpha=0.4)
+  plt.plot(Ns, Eouts, color='blue', alpha=0.4)
+  plt.legend(['Ein', 'Eout'])
+  plt.show()
+
+  plt.title('N VS Accuracy')
+  plt.xlabel('N')
+  plt.ylabel('Accuracy')
+
+  # Show the plot
+  plt.plot(Ns, accs, color='green', alpha=0.4)
+  plt.legend(['Accuracy'])
+  plt.show()
+
 def q10():
   Eins, Eouts, accs = [], [], []
-  for i in range(1):
-    Ein, Eout, acc = linearRegression(16, 1000)
+  features, sample_size, iterations = 16, 5000, 1
+
+  x, y, test_x = preprocess_data(data, features)
+
+  for i in range(iterations):
+    Ein, Eout, acc = linearRegression(x, y, test_x, features, sample_size)
     Eins.append(Ein)
     Eouts.append(Eout)
     accs.append(acc)
-    print(acc)
     
   print('average accuracy:', np.mean(accs))
   print('max accuracy:', np.max(accs))
@@ -215,22 +243,17 @@ def q10():
   # Show the plot
   plt.scatter(Eins, Eouts, color='blue', alpha=0.4)
 
-
-
 def q11():
-  N = 25
-  Ns = []
-  Eins = []
-  Eouts = []
-  accs = []
+  Ns, Eins, Eouts, accs = [], [], [], []
+  features, N = 7, 25
 
-  while N < 10001:
-    EinSum = 0
-    EoutSum = 0
-    accSum = 0
+  x, y, test_x = preprocess_data(data, features)
+
+  while N < 9001:
+    EinSum, EoutSum, accSum = 0, 0, 0
 
     for i in range(16):
-      Ein, Eout, acc = linearRegression(163, N)
+      Ein, Eout, acc = linearRegression(x, y, test_x, features, N)
       EinSum += Ein
       EoutSum += Eout
       accSum += acc
@@ -246,31 +269,19 @@ def q11():
 
     N = N + 25
 
-  plt.title('N VS Ein and Eout')
-  plt.xlabel('N')
-  plt.ylabel('Ein and Eout')
-
-  # Show the plot
-  plt.plot(Ns, Eins, color='red', alpha=0.4)
-  plt.plot(Ns, Eouts, color='blue', alpha=0.4)
-  plt.plot(Ns, accs, color='green', alpha=0.4)
-  plt.legend(['Ein', 'Eout', 'Accuracy'])
-  plt.show()
+  plot_output(Ns, Eins, Eouts, accs)
 
 def q12():
-  N = 25
-  Ns = []
-  Eins = []
-  Eouts = []
-  accs = []
+  Ns, Eins, Eouts, accs = [], [], [], []
+  features, N = 2, 25
 
-  while N < 10001:
-    EinSum = 0
-    EoutSum = 0
-    accSum = 0
+  x, y, test_x = preprocess_data(data, features)
+
+  while N < 9001:
+    EinSum, EoutSum, accSum = 0, 0, 0
 
     for i in range(16):
-      Ein, Eout, acc = linearRegression(2, N)
+      Ein, Eout, acc = linearRegression(x, y, test_x, features, N)
       EinSum += Ein
       EoutSum += Eout
       accSum += acc
@@ -286,15 +297,33 @@ def q12():
 
     N = N + 25
 
-  plt.title('N VS Ein and Eout')
-  plt.xlabel('N')
-  plt.ylabel('Ein and Eout')
+  plot_output(Ns, Eins, Eouts, accs)
 
-  # Show the plot
-  plt.plot(Ns, Eins, color='red', alpha=0.4)
-  plt.plot(Ns, Eouts, color='blue', alpha=0.4)
-  plt.plot(Ns, accs, color='green', alpha=0.4)
-  plt.legend(['Ein', 'Eout', 'Accuracy'])
-  plt.show()
+def q13():
+  features, Eins, Eouts, accs = [], [], [], []
+  feature, N = 2, 5000
 
-q10()
+  while feature < 30:
+    x, y, test_x = preprocess_data(data, feature)
+    EinSum, EoutSum, accSum = 0, 0, 0
+
+    for i in range(16):
+      Ein, Eout, acc = linearRegression(x, y, test_x, feature, N)
+      EinSum += Ein
+      EoutSum += Eout
+      accSum += acc
+
+    EinSum /= 16
+    EoutSum /= 16
+    accSum /= 16
+
+    features.append(feature)
+    Eins.append(EinSum)
+    Eouts.append(EoutSum)
+    accs.append(accSum)
+
+    feature = feature + 1
+
+  plot_output(features, Eins, Eouts, accs)
+
+q11()
